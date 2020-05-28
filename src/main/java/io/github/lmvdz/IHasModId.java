cecd211ef18557f47e4fca97bd744537aece1610
Lars van der Zande <xeonpowder@gmail.com>
2020-05-28 00:51:55 -0500

    latest

---
+++ src/main/java/io/github/lmvdz/IHasModId.java
@@ -0,0 +1,5 @@
+package io.github.lmvdz;
+
+public interface IHasModId {
+    String getModId();
+}
---
+++ src/main/java/io/github/lmvdz/SpashScreenMod.java
@@ -0,0 +1,17 @@
+package io.github.lmvdz;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SpashScreenMod implements ModInitializer, IHasModId {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+    @Override
+    public String getModId() {
+        return "splashscreenmod";
+    }
+
+}
